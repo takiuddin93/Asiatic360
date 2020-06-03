d07bb7f240aea56bde670de6f856e22869a29c8d
Taki Uddin <takiuddin93@gmail.com>
2020-06-03 16:41:52 +0600

    universalvariables.dart file added

---
+++ lib/utils/universal_variables.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class UniversalVariables {
+  static final Color transparent = Color(0x00FFFFFF);
+  static final Color primaryCrimson = Color(0xFFDA2323);
+  static final Color black = Color(0xFF000000);
+}
